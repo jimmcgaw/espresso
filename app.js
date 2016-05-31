d6975cc1dfe302c69cf23c4edb20c8ffdb762716
jimmcgaw <jpmcgaw@gmail.com>
2016-05-31 10:54:10 -0700

    add gulp tasks for compiling sass, wiredep, bowerfile for dependencies, and injection of stuff into our base index.html file

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 
 var app = express();
 
-var port = 5000;
+var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 app.use(express.static('src/views'));
@@ -11,7 +11,7 @@ app.use(express.static('src/views'));
 
 app.get('/', function(request, response){
     response.send('hello world');
-})
+});
 
 app.listen(port, function(err){
     console.log('Server running on port: ' + port.toString());
---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var jscs = require('gulp-jscs');
+var nodemon = require('nodemon');
+
+var jsFiles = ['*.js', 'src/**/*.js'];
+
+gulp.task('default', ['sass:watch']);
+
+gulp.task('sass', function () {
+  return gulp.src('./public/sass/styles.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./public/sass/**/*.scss', ['sass', 'inject']);
+});
+
+gulp.task('jshint', function(){
+   return gulp.src(jsFiles)
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish', {
+       verbose: true
+   }))
+   .pipe(jscs());
+});
+
+gulp.task('inject', function(){
+    var wiredep = require('wiredep').stream;
+    var inject = require('gulp-inject');
+    
+    var injectSrc = gulp.src(['./public/css/*.css', 
+                             './public/js/*.js'], {read: false});
+    var injectOptions = {
+        ignorePath: '/public'
+    };
+    
+    var options = {
+        bowerJson: require('./bower.json'),
+        directory: './public/lib',
+        ignorePath: '../../public'
+    }
+    
+    return gulp.src('./src/views/*.html')
+        .pipe(wiredep(options))
+        .pipe(inject(injectSrc, injectOptions))
+        .pipe(gulp.dest('./src/views'));
+        
+});
+
+gulp.task('serve', ['jshint', 'inject'], function(){
+    var options = {
+        script: 'app.js',
+        delayTime: 1,
+        env: {
+            'PORT': 3000
+        },
+        watch: jsFiles
+    };
+    
+    return nodemon(options)
+        .on('restart', function(ev){
+            console.log('Restarting...');
+        });
+})
