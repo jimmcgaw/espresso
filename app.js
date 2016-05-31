d953aaadbd872999c2317b000864cad251cfd8d5
jimmcgaw <jpmcgaw@gmail.com>
2016-05-31 09:09:18 -0700

    hello node and express development + bootstrap alpha 4

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+
+
+var app = express();
+
+var port = 5000;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+
+app.get('/', function(request, response){
+    response.send('hello world');
+})
+
+app.listen(port, function(err){
+    console.log('Server running on port: ' + port.toString());
+});
